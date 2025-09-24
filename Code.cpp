748dd5000632261529003de653381f919ef9182f
Abdullah Sailan <80727363+Abdullah21k@users.noreply.github.com>
2025-09-23 20:36:22 -0400

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,20 +1,20 @@
-String password = "ABCDEF";
+String password = "ABCDEF"; //Desired password
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //begin the conversation
 }
 
 void loop() {
-  Serial.println("Please enter your password: ");
+  Serial.println("Please enter your password: "); //Enter the first command to the user
 
-  while(Serial.available() == 0){
+  while(Serial.available() == 0){ //so long as no input keep here
     }
 
-  String input = Serial.readString();
+  String input = Serial.readString(); //Store input here
 
-  if(input == password){
+  if(input == password){ //If input is the same as password, print password correct
     Serial.println("Password correct");
   }
     else
-      Serial.println("Password incorrect");
+      Serial.println("Password incorrect"); //else incorrect
 }
